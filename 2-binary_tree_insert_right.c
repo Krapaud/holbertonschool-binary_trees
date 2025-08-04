0dc0776266577be7616c355bb993d26609b1a0f6
Krapaud <mur.mickael@gmail.com>
2025-08-04 14:31:32 +0200

    Fix binary tree insert right function and update header file for structure definitions

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -6,12 +6,23 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	binary_tree_t *new;
 
 	if (parent == NULL)
-	return (NULL);
+		return (NULL);
 
 	new = binary_tree_node(parent, value);
 
-	if (new = NULL)
+	if (new == NULL)
 		return (NULL);
 
+	if (parent->right == NULL)
+	{
+		parent->right = new;
+	}
+	else
+	{
+		new->right = parent->right;
+		parent->right->parent = new;
+		parent->right = new;
+		new->parent = parent;
+	}
 	return (new);
 }
