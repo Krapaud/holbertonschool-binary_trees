09a9aa7ca3d11e130e9473f06a5ce12c6608c78d
Krapaud <mur.mickael@gmail.com>
2025-08-05 10:41:00 +0200

    Fix binary_tree_is_perfect function to return 0 for NULL tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -32,7 +32,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (1);
+		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
