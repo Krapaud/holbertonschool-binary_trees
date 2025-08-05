d918a430f65a84022c1318204f4229b7a3b03849
Krapaud <mur.mickael@gmail.com>
2025-08-05 11:19:38 +0200

    Fix return value in binary_tree_height function for NULL tree case

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,9 +11,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t rHeight = 0;
 
 	if (tree == NULL)
-		return (-1);
-
-	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
 	lHeight = binary_tree_height(tree->left);
